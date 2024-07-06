bf41b8d27510d3fcb8c9ca6fc10e4f05907362aa
Leon Groschek <mail@leon-groschek.de>
2024-07-06 15:40:40 +0200

    add task start style

--- src/app/maincomponents/maincontent/addtask/addtask.component.ts
+++ src/app/maincomponents/maincontent/addtask/addtask.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addtask',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './addtask.component.html',
   styleUrl: './addtask.component.scss'
 })
 export class AddtaskComponent {
 
+  taskForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      date: ['', Validators.required],
+      priority: ['', Validators.required],
+      assignedTo: [''],
+      category: [''],
+      subtasks: ['']
+    });
+  }
+
+  clearForm(): void {
+    this.taskForm.reset();
+  }
+
+  createTask(): void {
+    if (this.taskForm.valid) {
+      console.log('Formulardaten:', this.taskForm.value);
+      this.clearForm();
+    } else {
+      alert('Formular ist nicht gültig');
+    }
+  }
 }
